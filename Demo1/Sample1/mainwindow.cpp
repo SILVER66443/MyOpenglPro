713a172cee57a9d159afe979667595cacdaf9522
SILVER66443 <594846313@qq.com>
2022-05-20 17:15:12 +0800

    demo1

--- Demo1/Sample1/mainwindow.cpp
+++ Demo1/Sample1/mainwindow.cpp
@@ -3,13 +3,12 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    setCentralWidget(ui->openGLWidget);
 }
 
-MainWindow::~MainWindow()
-{
+
+MainWindow::~MainWindow() {
     delete ui;
 }
-
